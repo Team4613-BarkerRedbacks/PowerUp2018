26a2265692a13f68106fc5d14da2602b89fd9a9c
B-e-n-S <b3nschwarz@gmail.com>
2018-01-17 18:11:34 +1100

    Added initial code
    Added OI and Robot classes

---
+++ org/usfirst/frc/team4613/robot/OI.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team4613.robot;
+
+import redbacks.arachne.core.OIBase;
+
+public class OI extends OIBase {
+
+	@Override
+	public void mapOperations() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ org/usfirst/frc/team4613/robot/Robot.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team4613.robot;
+
+import redbacks.arachne.core.ArachneRobot;
+import redbacks.arachne.lib.commands.CommandBase;
+
+public class Robot extends ArachneRobot {
+public static OI oi = new OI();
+	@Override
+	public void initDefaultCommands() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public CommandBase getAutonomous(int autoID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
