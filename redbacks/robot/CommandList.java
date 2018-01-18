ae2ddc30e4c5c1840c196458ced9955025d0666b
parkerl20 <31122263+parkerl20@users.noreply.github.com>
2018-01-18 14:34:03 +1100

    Integration
    Integration of Subsystem CommandLists into the CommandList

--- redbacks/robot/CommandList.java
+++ redbacks/robot/CommandList.java
@@ -1,11 +1,36 @@
 package redbacks.robot;
 
 import redbacks.arachne.core.references.CommandListStart;
+import redbacks.arachne.lib.actions.actuators.AcMotor;
+import redbacks.arachne.lib.checks.ChFalse;
+import redbacks.arachne.lib.checks.analog.ChNumSen;
 import redbacks.arachne.lib.commands.CommandSetup;
 import redbacks.robot.actions.AcDrive;
 
 public class CommandList extends CommandListStart {
+	
 	static {subsystemToUse = Robot.driver;}
 	public static CommandSetup
 		drive = newCom(new AcDrive());
+	
+	static {subsystemToUse = Robot.arm;}
+	public static CommandSetup
+		moveArm 	= newCom(new AcMotor.Set(Robot.arm.aMotor, 0.3, new ChFalse())),
+		reverseArm 	= newCom(new AcMotor.Set(Robot.arm.aMotor, -0.3, new ChFalse())),
+		setArmFlatR = newCom(new AcMotor.Set(Robot.arm.aMotor, 0.3, new ChNumSen(950, Robot.sensors.armEncoder, true, false, false))),
+		setArmFlatL = newCom(new AcMotor.Set(Robot.arm.aMotor, 0.3, new ChNumSen(-950, Robot.sensors.armEncoder, true, false, false))),
+		setArm550R 	= newCom(new AcMotor.Set(Robot.arm.aMotor, 0.3, new ChNumSen(550, Robot.sensors.armEncoder, true, false, false))),
+		setArm550L 	= newCom(new AcMotor.Set(Robot.arm.aMotor, 0.3, new ChNumSen(-550, Robot.sensors.armEncoder, true, false, false))),
+		setArm300R 	= newCom(new AcMotor.Set(Robot.arm.aMotor, 0.3, new ChNumSen(300, Robot.sensors.armEncoder, true, false, false))),
+		setArm300L 	= newCom(new AcMotor.Set(Robot.arm.aMotor, 0.3, new ChNumSen(-300, Robot.sensors.armEncoder, true, false, false))),
+		centreArm 	= newCom(new AcMotor.Set(Robot.arm.aMotor, 0.3, new ChNumSen(0, Robot.sensors.armEncoder, true, false, false)));
+	
+	static {subsystemToUse = Robot.climber;}
+	public static CommandSetup
+		climb = newCom(new AcMotor.Set(Robot.climber.climberMotor, 0.25, new ChFalse()));
+	
+	static {subsystemToUse = Robot.intake;}
+	public static CommandSetup
+		intake = newCom(new AcMotor.Set(Robot.intake.intakeRightMotor, 0.25, new ChFalse()), (new AcMotor.Set(Robot.intake.intakeLeftMotor, 0.25, new ChFalse()))),
+		outtake = newCom(new AcMotor.Set(Robot.intake.intakeRightMotor, -0.25, new ChFalse()), (new AcMotor.Set(Robot.intake.intakeRightMotor, -0.25, new ChFalse())));
 }
