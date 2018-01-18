85313a4bcab56eeb78f01fc72491bd1db40d70bf
parkerl20 <31122263+parkerl20@users.noreply.github.com>
2018-01-18 13:46:48 +1100

    Subsystem Intake Completion
    Establishment of Subsystem Intake
    
    Minor additions to new Subsystem Climber

---
+++ redbacks/robot/subsystems/SubsystemClimber.java
@@ -0,0 +1,11 @@
+package redbacks.robot.subsystems;
+
+import redbacks.arachne.core.SubsystemBase;
+
+public class SubsystemClimber extends SubsystemBase {
+	
+	
+	public SubsystemClimber() {
+		super();
+	}
+}
