2e9ac818da62aae2653bed8564081b36276c6fde
SMEZ1234 <smez12345@gmail.com>
2018-02-13 20:21:18 +1100

    Added TankDrive class from previous years

---
+++ redbacks/robot/actions/AcTankDrive.java
@@ -0,0 +1,28 @@
+package redbacks.robot.actions;
+
+import redbacks.arachne.lib.actions.Action;
+import redbacks.arachne.lib.checks.Check;
+import redbacks.robot.Robot;
+
+public class AcTankDrive extends Action
+{
+	double l, r;
+	
+	public AcTankDrive(Check check, double lSpeed, double rSpeed) {
+		super(check);
+		l = lSpeed;
+		r = rSpeed;
+	}
+	
+	public void onStart() {
+		Robot.isIndivDriveControl = false;
+	}
+
+	public void onRun() {
+		Robot.driver.drivetrain.tankDrive(-l, -r);
+	}
+
+	public void onFinish() {
+		onRun();
+	}
+}
