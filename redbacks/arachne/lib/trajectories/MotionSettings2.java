7b510f32e015c32d613af6d4d04dacbd9e327848
SMEZ1234 <smez12345@gmail.com>
2018-02-13 20:20:05 +1100

    Moved PID values to MotionSettings2

---
+++ redbacks/arachne/lib/trajectories/MotionSettings2.java
@@ -0,0 +1,15 @@
+package redbacks.arachne.lib.trajectories;
+
+import redbacks.arachne.ext.motion.MotionSettings;
+
+/**
+ * 
+ *
+ * @author Sean Zammit
+ */
+public class MotionSettings2 extends MotionSettings
+{
+	public static final double
+		drivePIDMotorkP = 2.0E-5, drivePIDMotorkI = 1.0E-8, drivePIDMotorkD = 7.0E-6,
+		drivePIDGyrokP = 4.0E-2, drivePIDGyrokI = 1.0E-5, drivePIDGyrokD = 1.0E-2;
+}
