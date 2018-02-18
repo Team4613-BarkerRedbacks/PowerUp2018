4fe2f67f6a16d21c29a4cc5fbea02e7f775a12ca
SMEZ1234 <smez12345@gmail.com>
2018-02-18 13:05:42 +1100

    Increased P for path following

--- redbacks/arachne/lib/override/MotionSettings2.java
+++ redbacks/arachne/lib/override/MotionSettings2.java
@@ -10,6 +10,6 @@ import redbacks.arachne.ext.motion.MotionSettings;
 public class MotionSettings2 extends MotionSettings
 {
 	public static final double
-		drivePIDMotorkP = 2.0E-5, drivePIDMotorkI = 1.0E-8, drivePIDMotorkD = 7.0E-6,
+		drivePIDMotorkP = 4.0E-5, drivePIDMotorkI = 1.0E-8, drivePIDMotorkD = 7.0E-6,
 		drivePIDGyrokP = 4.0E-2, drivePIDGyrokI = 3.0E-6, drivePIDGyrokD = 7.0E-3;
 }
