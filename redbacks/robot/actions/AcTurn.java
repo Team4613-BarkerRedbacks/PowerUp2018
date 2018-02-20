86bda6c8a56485109184d87683f50a2253e58491
SMEZ1234 <smez12345@gmail.com>
2018-02-20 19:40:24 +1100

    Avoided ChMulti bug on time

--- redbacks/robot/actions/AcTurn.java
+++ redbacks/robot/actions/AcTurn.java
@@ -3,9 +3,8 @@ package redbacks.robot.actions;
 import redbacks.arachne.core.ArachneRobot;
 import redbacks.arachne.ext.motion.pid.Tolerances;
 import redbacks.arachne.lib.checks.ChMulti;
-import redbacks.arachne.lib.checks.ChTime;
 import redbacks.arachne.lib.checks.analog.ChGettableNumber;
-import redbacks.arachne.lib.logic.LogicOperators;
+import redbacks.arachne.lib.logic.ListOperators;
 import redbacks.arachne.lib.trajectories.AcPath;
 import redbacks.arachne.lib.trajectories.Path;
 import redbacks.robot.Robot;
@@ -18,10 +17,10 @@ public class AcTurn extends AcPath
 {
 	public AcTurn(double angleTarget) {
 		super(new ChMulti(
-				LogicOperators.AND,
-				new ChTime(0.2),
+				ListOperators.ORDER,
+				new ChGettableNumber(RobotMap.stoppedTurnThreshold * 10, Robot.sensors.rateYaw, true, true),
 				new ChGettableNumber(RobotMap.stoppedTurnThreshold, Robot.sensors.rateYaw, false, true)
-		), false, new Path(new double[]{0, angleTarget, 0}), Robot.driver.drivetrain, 1, 1, Robot.sensors.yaw, Robot.sensors.driveCentreEncoder, false, new Tolerances.Absolute(5));
+		), false, new Path(new double[]{0, angleTarget, 0}), Robot.driver.drivetrain, 1, 1, Robot.sensors.yaw, Robot.sensors.driveCentreEncoder, false, new Tolerances.Absolute(0));
 	}
 	
 	public void onStart() {
@@ -35,5 +34,7 @@ public class AcTurn extends AcPath
 		acRotation.execute();
 		
 		drivetrain.tankDrive(rotationOut.output, - rotationOut.output);
+		
+		System.out.println(check.isFinished());
 	}
 }
