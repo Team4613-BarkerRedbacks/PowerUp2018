1cfe554791590609f04bc56be3c244386b9b2244
SMEZ1234 <smez12345@gmail.com>
2018-03-06 20:18:07 +1100

    Added timer safety to AcTurn

--- redbacks/robot/actions/AcTurn.java
+++ redbacks/robot/actions/AcTurn.java
@@ -6,6 +6,7 @@ import redbacks.arachne.lib.checks.ChMulti;
 import redbacks.arachne.lib.checks.analog.ChGettableNumber;
 import redbacks.arachne.lib.checks.analog.ChNumSen;
 import redbacks.arachne.lib.logic.ListOperators;
+import redbacks.arachne.lib.logic.LogicOperators;
 import redbacks.arachne.lib.sensors.SenTimer;
 import redbacks.arachne.lib.trajectories.AcPath;
 import redbacks.arachne.lib.trajectories.Path;
@@ -20,7 +21,10 @@ public class AcTurn extends AcPath
 	public AcTurn(double angleTarget) {
 		super(new ChMulti(
 				ListOperators.ORDER,
-				new ChGettableNumber(RobotMap.stoppedTurnThreshold * 10, Robot.sensors.rateYaw, true, true),
+				new ChMulti(LogicOperators.OR,
+						new ChGettableNumber(RobotMap.stoppedTurnThreshold * 10, Robot.sensors.rateYaw, true, true),
+						new ChNumSen(0.5, new SenTimer())
+				),
 				new ChNumSen(0.5, new SenTimer()),
 				new ChGettableNumber(RobotMap.stoppedTurnThreshold, Robot.sensors.rateYaw, false, true)
 		), false, new Path(new double[]{0, angleTarget, 0}), Robot.driver.drivetrain, 1, 1, Robot.sensors.yaw, Robot.sensors.driveCentreEncoder, false, new Tolerances.Absolute(0));
