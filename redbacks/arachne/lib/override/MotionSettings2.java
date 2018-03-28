107661891f188efce301a4578e9f91114c3e54b6
SMEZ1234 <smez12345@gmail.com>
2018-03-28 14:11:20 +1100

    Added changelog to MotionSettings2 min voltage

--- redbacks/arachne/lib/override/MotionSettings2.java
+++ redbacks/arachne/lib/override/MotionSettings2.java
@@ -13,5 +13,14 @@ public class MotionSettings2 extends MotionSettings
 		drivePIDMotorkP = 4.0E-5, drivePIDMotorkI = 1.0E-8, drivePIDMotorkD = 7.0E-6,
 		//gyro kp 4 -> 3
 		drivePIDGyrokP = 4.0E-2, drivePIDGyrokI = 3.0E-6, drivePIDGyrokD = 7.0E-3,
-		driveMinVoltage = 0.35, driveSlowVoltage = 0.4;
+		
+		/**
+		 * This variable has a running changelog. Do not edit it without adding an entry.
+		 * 23/3: Set to initial value of 0.35
+		 * 28/3: 0.35 -> 0.3 -> 0.2 -> 0.35 -> 0.3
+		 */
+		driveMinVoltage = 0.35,
+		
+		
+		driveSlowVoltage = 0.4;
 }
