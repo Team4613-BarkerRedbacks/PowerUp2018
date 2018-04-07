2ad39bffdb2c83923e264da9697f8cb145f9f36c
SMEZ1234 <smez12345@gmail.com>
2018-04-07 14:50:59 +1000

    Cleared secondary auto file

--- redbacks/robot/Auto2.java
+++ redbacks/robot/Auto2.java
@@ -34,60 +34,6 @@ public class Auto2
 {
 	public static CommandBase getAutoComponent(AutoComponent autoComponent) {
 		switch(autoComponent) {
-		
-		case R_RR_LHH_65:
-			return createAuto(
-					new AcResetSensors(),
-					//1st cube
-					new AcSeq.Parallel(
-							new AcDoNothing(new ChNumSen(1.5 * encoderTicksPerMetre, sensors.distanceEncoder)),
-							new AcSeq.Parallel(solExtendIntakeL),
-							new AcWait(1),
-							new AcSeq.Parallel(solRetractIntakeL),
-							new AcDoNothing(new ChNumSen(5 * encoderTicksPerMetre, sensors.distanceEncoder, true, false, false)),
-							new AcSetArm(-armBasePos),
-							new AcSeq.Parallel(intake, new AcSplitIntakeControl(new ChFalse(), intakeFastSpeed, intakeSlowSpeed))
-					),
-					new AcStraight(4, -11, sensors.distanceEncoder, true),
-					//2nd cube
-					new AcStraight(5.8, 5, sensors.distanceEncoder, false),
-					new AcTurn(40),
-					new AcDoNothing(new ChNumSen(-armBasePos + 100, sensors.armEncoder, false, false, false)),
-					new AcStraightLenient(-1.2, 40, sensors.distanceEncoder, true),
-					new AcTankDrive(new ChTime(0.25), 0.5, 0.5),
-					new AcWait(0.25),
-					new AcSeq.Parallel(
-							new AcSetArm(armSlightPosTele),
-							new AcSeq.Parallel(highFirePrime)
-					),
-					new AcTurn(-5),
-					new AcInterrupt.KillSubsystem(intake),
-					new AcSeq.Parallel(
-							new AcDoNothing(new ChNumSen(1 * encoderTicksPerMetre, sensors.distanceEncoder, true, false, false)),
-							new AcSeq.Parallel(highFireRelease)
-					),
-					new AcStraight(1.25, -5, sensors.distanceEncoder, true),
-					//3rd cube
-					new AcWait(0.25),
-					new AcSetArm(-armBasePos),
-					new AcDriveDirection(new ChNumSen(-0.6 * encoderTicksPerMetre, sensors.averageEncoder), -1, -5),
-					new AcTankTurn(35),
-					new AcSeq.Parallel(intakeCubeFast),
-					new AcStraightLenient(-0.8, 35, sensors.distanceEncoder, true),
-					new AcTankDrive(new ChTime(0.25), -0.5, -0.5),
-					new AcSeq.Parallel(
-							new AcDoNothing(new ChNumSen(1 * encoderTicksPerMetre, sensors.averageEncoder, false, false, true)),
-							new AcSeq.Parallel(highFirePrime)
-					),
-					new AcSetArm(armScalePos),
-					new AcSeq.Parallel(intakeCubeSlow),
-					new AcDriveDirection(new ChNumSen(0.8 * encoderTicksPerMetre, sensors.distanceEncoder), 1, 35),
-					new AcTankTurn(-5),
-					new AcInterrupt.KillSubsystem(intake),
-					new AcDriveDirection(new ChNumSen(0.5 * encoderTicksPerMetre, sensors.distanceEncoder), 1, -5),
-					new AcWait(0.25),
-					new AcSeq.Parallel(highFireRelease)	
-				);
 			default: return null;
 		}
 	}
