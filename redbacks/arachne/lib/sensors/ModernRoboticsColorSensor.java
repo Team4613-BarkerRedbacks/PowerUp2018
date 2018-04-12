a9db891d682699ad1d37938a4abb36c3fe3ceeec
Matthew <matt@brians.com.au>
2018-04-12 14:49:36 +1000

    Colour Sensing Code?
    It's reading something? The values read the wrong colour and are to the power of -23 but it seems to work? Maybe. Centre Encoder Number also changed to reflect robot changes

--- redbacks/arachne/lib/sensors/ModernRoboticsColorSensor.java
+++ redbacks/arachne/lib/sensors/ModernRoboticsColorSensor.java
@@ -5,12 +5,12 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.SensorBase;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class ModernRoboticsColorSensor extends SensorBase
-{
+public class ModernRoboticsColorSensor extends SensorBase {
 	private static final byte
 		kAddress = 0x1E,
 		
@@ -31,7 +31,11 @@ public class ModernRoboticsColorSensor extends SensorBase
 		kRedValRegister = 0x05,
 		kGreenValRegister = 0x06,
 		kBlueValRegister = 0x07,
-		kWhiteValRegister = 0x08
+		kWhiteValRegister = 0x08,
+		
+		RedValRegister = 0x16,
+		GreenValRegister = 0x18,
+		BlueValRegister = 0x1a
 	;
 	
 	protected I2C m_i2c;
@@ -61,9 +65,9 @@ public class ModernRoboticsColorSensor extends SensorBase
 	}
 
 	public static enum ColorComponent {
-		RED(kRedValRegister),
-		GREEN(kGreenValRegister),
-		BLUE(kBlueValRegister),
+		RED(RedValRegister),
+		GREEN(GreenValRegister),
+		BLUE(BlueValRegister),
 		ALPHA(kWhiteValRegister);
 		
 		public byte register;
@@ -153,12 +157,13 @@ public class ModernRoboticsColorSensor extends SensorBase
 	}
 
 	public double getColorValue(ColorComponent color) {
-		ByteBuffer rawColor = ByteBuffer.allocate(2);
 		
-		m_i2c.read(color.register, 2, rawColor);
+		ByteBuffer rawColor = ByteBuffer.allocate(8);
+		
+		m_i2c.read(color.register, 8, rawColor);
 
 		rawColor.order(ByteOrder.BIG_ENDIAN);
-		return rawColor.getShort(0);
+		return rawColor.getDouble();
 	}
 
 	@Override
