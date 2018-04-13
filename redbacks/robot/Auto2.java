0763c450a6561fee2ef5473f618b695228698705
SMEZ1234 <smez12345@gmail.com>
2018-04-13 11:42:09 +1000

    Revert "Added L-RLLL_43 auto (have not tested)"
    This reverts commit 0913edba3fb8d6cc3790045e2aceff30dcd28c1b.

--- redbacks/robot/Auto2.java
+++ redbacks/robot/Auto2.java
@@ -15,10 +15,8 @@ import redbacks.arachne.lib.actions.AcSetNumSen;
 import redbacks.arachne.lib.actions.AcWait;
 import redbacks.arachne.lib.checks.ChFalse;
 import redbacks.arachne.lib.checks.ChTime;
-import redbacks.arachne.lib.checks.ChTrue;
 import redbacks.arachne.lib.checks.analog.ChNumSen;
 import redbacks.arachne.lib.commands.CommandBase;
-import redbacks.arachne.lib.sensors.SenTimer;
 import redbacks.robot.Auto.AutoComponent;
 import redbacks.robot.actions.AcDriveDirection;
 import redbacks.robot.actions.AcResetSensors;
@@ -35,42 +33,8 @@ import static redbacks.robot.Auto.*;
 
 public class Auto2
 {
-	public static enum AutoComponent {
-		 L_RLLL_43;
-	}
 	public static CommandBase getAutoComponent(AutoComponent autoComponent) {
 		switch(autoComponent) {
-		case L_RLLL_43:
-			return createAuto(
-					new AcResetSensors(),
-					//1st cube
-					new AcStraight(5.45, 0, sensors.distanceEncoder, true),
-					new AcTurn(90),
-					new AcStraight(2.45, 90, sensors.distanceEncoder, true),
-					new AcTurn(135),
-					new AcSetArm(armSwitchPos),
-					new AcSeq.Parallel(sequencer,
-							new AcStraight(1.2, 135, sensors.distanceEncoder, true)
-					),
-					new AcDoNothing(new ChNumSen(1.5, new SenTimer())),
-					new AcInterrupt.KillSubsystem(sequencer),
-					new AcSeq.Parallel(outtakeCubeFast),
-					//2nd cube
-					new AcWait(0.25),
-					new AcInterrupt.KillSubsystem(intake),
-					new AcDriveDirection(new ChTime(0.75), -0.6, 135),
-					new AcSetArm(armBasePos),
-					new AcSeq.Parallel(intake, new AcSplitIntakeControl(new ChFalse(), intakeSlowSpeed, intakeFastSpeed)),
-					new AcDriveDirection(new ChTime(1.25), 0.6, 145),
-					new AcWait(0.5),
-					new AcSeq.Parallel(highFirePrime),
-					new AcSetArm(armSwitchPos - 100),
-					new AcDoNothing(new ChNumSen(armSwitchPos, sensors.armEncoder, false, false, false)),
-					new AcInterrupt.KillSubsystem(intake),
-					new AcWait(0.25),
-					new AcSeq.Parallel(
-							CommandList.newFireCommand(null, new AcDoNothing(new ChTrue()))
-					));
 			default: return null;
 		}
 	}
